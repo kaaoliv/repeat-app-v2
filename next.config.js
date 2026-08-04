862bc5cdd595455aeb591a2d73b7e65189e1c622
kaaoliv <kaikefoliv@gmail.com>
2026-08-04 15:36:11 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -16,6 +16,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "lastfm.freetls.fastly.net",
+      },
+      {
+        protocol: "https",
         hostname: "lastfm.freetls.fmcdn.net",
       },
     ],
